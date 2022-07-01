7704137548c6e6ad3541f96c6496381ccdb57f72
Zoro <strawhatzoro1993@gmail.com>
2022-07-01 13:53:15 -0400

    [ADD] facebook auth

---
+++ src/facebook.strategy.ts
@@ -0,0 +1,30 @@
+import { Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { Profile, Strategy } from "passport-facebook";
+
+@Injectable()
+export class FacebookStrategy extends PassportStrategy(Strategy, "facebook") {
+    constructor() {
+        super({
+            clientID: process.env.APP_ID,
+            clientSecret: process.env.APP_SECRET,
+            callbackURL: "http://localhost:3000/facebook/redirect",
+            scope: "email",
+            profileFields: ["emails", "name"],
+        });
+    }
+
+    async validate(
+        accessToken: string,
+        refreshToken: string,
+        profile: Profile,
+        done: (err: any, user: any, info?: any) => void
+    ): Promise<any> {
+        const payload = {
+            profile,
+            accessToken,
+        };
+
+        done(null, payload);
+    }
+}
