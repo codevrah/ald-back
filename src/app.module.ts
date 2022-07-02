5110cc07fba560883ed9fc954948a7054721cbe5
Zoro <strawhatzoro1993@gmail.com>
2022-07-01 23:01:52 -0400

    [ADD] env variables for mongodb connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,19 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { FacebookStrategy } from "./facebook.strategy";
-import { VotesModule } from './votes/votes.module';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {FacebookStrategy} from "./facebook.strategy";
+import {VotesModule} from './votes/votes.module';
 import {MongooseModule} from "@nestjs/mongoose";
 import {VotesService} from "./votes/votes.service";
 
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://admin:admin@127.0.0.1:27017/ald_db'), VotesModule],
+  imports: [MongooseModule.forRoot(`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`), VotesModule],
   controllers: [AppController],
   providers: [AppService, FacebookStrategy],
 })
-export class AppModule {}
+export class AppModule {
+}
